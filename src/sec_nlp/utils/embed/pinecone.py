925635a7dfad2f9f2025f318b4d47fcecbec43ee
Nic Hill <nrhill1@gmail.com>
2025-09-19 12:31:44 -0500

    starting add_index

--- src/sec_nlp/utils/embed/pinecone.py
+++ src/sec_nlp/utils/embed/pinecone.py
@@ -10,5 +10,9 @@ logger = logging.getLogger(__name__)
 
 class PineconeDBManager:
 
-    def __init__(self, pinecone_api_key: str):
+    def __init__(self, pinecone_api_key: str = os.getenv("PINECONE_API_KEY")):
         self._pc = Pinecone(api_key=pinecone_api_key)
+
+    def add_index(self, index_name: str):
+        if not self._pc.has_index(index_name):
+            pass
