f2fc14ceb579ded81535964a3eaefdf3fc158b2e
Nic Hill <nrhill1@gmail.com>
2025-09-20 16:17:59 -0500

    add query

--- src/sec_nlp/embeddings/pinecone.py
+++ src/sec_nlp/embeddings/pinecone.py
@@ -11,10 +11,11 @@ logger = logging.getLogger(__name__)
 
 class PineconeEmbedder:
 
-    def __init__(self, pinecone_api_key: str = os.getenv("PINECONE_API_KEY")):
+    def __init__(self, pinecone_api_key: str, initial_index: str):
         self._pc = Pinecone(api_key=pinecone_api_key)
-        self._current_index = None
-        self._vector_store = None
+        self._current_index = self.set_index(
+            initial_index) if self._pc.has_index(initial_index) else self.add_index(initial_index)
+        self._vector_store = self.initialize_vector_store()
         logger.info("Initialized Pinecone client.")
 
     def add_documents(self, documents: list[Document]):
@@ -72,6 +73,25 @@ class PineconeEmbedder:
         self._vector_store = PineconeVectorStore(
             pinecone_index=self._current_index)
         logger.info("Initialized Pinecone vector store.")
+
+    def query(self, query: str, top_k: int = 5):
+        """
+        Query the Pinecone vector store.
+
+        Args:
+            query (str): Query string.
+            top_k (int, optional): Number of top results to return. Defaults to 5.
+
+        Raises:
+            ValueError: If the vector store is not initialized.
+
+        Returns:
+            list[Document]: List of retrieved documents.
+        """
+        results = self._vector_store.similarity_search(
+            query=query, k=top_k)
+        logger.info(f"Queried Pinecone vector store with query: {query}")
+        return results
 
     def set_index(self, index_name: str):
         """
