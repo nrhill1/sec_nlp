b3209d74720a3c255660d9ddf0c840b0cdacb306
Nic Hill <nrhill1@gmail.com>
2025-09-20 13:32:48 -0500

    change 'embed' module to 'embeddings' and add index methods to PineconeEmbedder

--- src/sec_nlp/embeddings/pinecone.py
+++ src/sec_nlp/embeddings/pinecone.py
@@ -8,22 +8,38 @@ from pinecone import Pinecone, ServerlessSpec
 logger = logging.getLogger(__name__)
 
 
-class PineconeVectorStore:
+class PineconeEmbedder:
 
     def __init__(self, pinecone_api_key: str = os.getenv("PINECONE_API_KEY")):
         self._pc = Pinecone(api_key=pinecone_api_key)
+        self._current_index = None
+        self._vector_store = None
 
     def add_index(self, index_name: str):
+        """
+        Create a new Pinecone index if it doesn't already exist.
+
+        Args:
+            index_name (str): Name of the Pinecone index to create
+        """
         if not self._pc.has_index(index_name):
             pc.create_index(
                 name=index_name,
-                dimension=1536,
-                metric="cosine",
+                embed={
+                    "model": "llama-text-embed-v2",
+                }
                 spec=ServerlessSpec(
                     cloud="aws",
                     region="us-east-1"
                 )
             )
+            self._current_index = self._pc.get_index(index_name)
             logger.info(f"Created Pinecone index: {index_name}")
         else:
             logger.info(f"Pinecone index already exists: {index_name}")
+
+    def set_index(self, index_name: str):
+        if not self._pc.has_index(index_name):
+            raise ValueError(f"Pinecone index does not exist: {index_name}")
+        self._current_index = self._pc.get_index(index_name)
+        logger.info(f"Current Pinecone index: {index_name}")
