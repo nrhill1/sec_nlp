7deb9b7cc8b1724ca959a011aa8730b21c69fa7e
Nic Hill <nrhill1@gmail.com>
2025-09-20 14:01:52 -0500

    complete add_documents

--- src/sec_nlp/embeddings/pinecone.py
+++ src/sec_nlp/embeddings/pinecone.py
@@ -1,9 +1,10 @@
 import logging
 import os
 
+from langchain_core import Document
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone, ServerlessSpec
-
+from uuid import uuid4
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +15,25 @@ class PineconeEmbedder:
         self._pc = Pinecone(api_key=pinecone_api_key)
         self._current_index = None
         self._vector_store = None
+        logger.info("Initialized Pinecone client.")
+
+    def add_documents(self, documents: list[Document]):
+        """
+        Add documents to the Pinecone vector store.
+
+        Args:
+            documents (list[Document]): List of documents to add.
+
+        Raises:
+            ValueError: If the vector store is not initialized.
+        """
+        if self._vector_store is None:
+            raise ValueError(
+                "Vector store not initialized. Please initialize the vector store before adding documents.")
+        uuids = [str(uuid4()) for _ in documents]
+        self._vector_store.add_documents(documents=documents, ids=uuids)
+        logger.info(
+            f"Added {len(documents)} documents to Pinecone vector store.")
 
     def add_index(self, index_name: str):
         """
@@ -25,6 +45,7 @@ class PineconeEmbedder:
         if not self._pc.has_index(index_name):
             pc.create_index(
                 name=index_name,
+                metric="cosine",
                 embed={
                     "model": "llama-text-embed-v2",
                 }
@@ -38,7 +59,30 @@ class PineconeEmbedder:
         else:
             logger.info(f"Pinecone index already exists: {index_name}")
 
+    def initialize_vector_store(self):
+        """
+        Initialize the Pinecone vector store.
+
+        Raises:
+            ValueError: If no Pinecone index is set.
+        """
+        if self._current_index is None:
+            raise ValueError(
+                "No Pinecone index set. Please set/add an index before initializing the vector store.")
+        self._vector_store = PineconeVectorStore(
+            pinecone_index=self._current_index)
+        logger.info("Initialized Pinecone vector store.")
+
     def set_index(self, index_name: str):
+        """
+        Set the current Pinecone index.
+
+        Args:
+            index_name (str): Name of the Pinecone index to set
+
+        Raises:
+            ValueError: If the specified Pinecone index does not exist.
+        """
         if not self._pc.has_index(index_name):
             raise ValueError(f"Pinecone index does not exist: {index_name}")
         self._current_index = self._pc.get_index(index_name)
